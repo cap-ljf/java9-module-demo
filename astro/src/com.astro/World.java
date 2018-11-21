11ee681ed06dc9ab396f5da7b488048cbf61df26
jifang.liu <jifang.liu@qunar.com>
2018-11-21 09:53:45 +0800

    java9 module demo

---
+++ astro/src/com.astro/World.java
@@ -0,0 +1,10 @@
+package com.astro;
+
+/**
+ * @author jifang.liu created on 2018/11/17 14:56
+ */
+public class World {
+    public static String name() {
+        return "world";
+    }
+}
---
+++ greetings/src/com.greetings/Main.java
@@ -0,0 +1,12 @@
+package com.greetings;
+
+import com.astro.World;
+
+/**
+ * @author jifang.liu created on 2018/11/17 14:44
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.format("Greetings %s", World.name());
+    }
+}
